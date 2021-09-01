5f5b4827c2113a1f0fbafff089e9dc35ac04d697
Veronica <veronicagoranova@gmail.com>
2021-09-01 22:39:40 +0300

    Adding solved problems

---
+++ Csharp-Advanced-September-2021/Stacks-And-Queues-Exercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Stack<char> openingBeackets = new Stack<char>(expression);
+            
+
+            bool flag = false;
+
+            foreach (var item in expression)
+            {
+                if (item == '(' || item == '[' || item == '{')
+                {
+                    openingBeackets.Push(item);
+                }
+                else
+                {
+                    char currentOpeningBracket = openingBeackets.Peek();
+
+                    if (currentOpeningBracket == '(' && item == ')')
+                    {
+                        openingBeackets.Pop();
+                    }
+                    else if (currentOpeningBracket == '[' && item == ']')
+                    {
+                        openingBeackets.Pop();
+                    }
+                    else if (currentOpeningBracket == '{' && item == '}')
+                    {
+                        openingBeackets.Pop();
+                    }
+                    else
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            
+            
+            if (expression.Length % 2 != 0 || expression.Length == 0)
+            {
+                flag = true;
+            }
+
+            if (flag)
+            {
+                Console.WriteLine("NO");
+            }
+            else 
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
