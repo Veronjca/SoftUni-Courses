0a58f5390c9efaf99afdaa149c44cd1aa75275b1
Veronica <veronicagoranova@gmail.com>
2021-09-07 12:53:39 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Multidimensional-Arrays-Lab/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] input = ReadArrayFromConsole();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {               
+                    sum += matrix[i, i];               
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
