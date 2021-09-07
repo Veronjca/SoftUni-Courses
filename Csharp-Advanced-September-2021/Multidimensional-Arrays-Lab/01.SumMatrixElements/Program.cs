1e853c3af2cade34773950a7b26de4cb40eabd20
Veronica <veronicagoranova@gmail.com>
2021-09-07 12:06:31 +0300

    Adding solved problem from "Multidimensional Arrays" lesson.

---
+++ Csharp-Advanced-September-2021/Multidimensional-Arrays-Lab/01.SumMatrixElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Multidimensional_Arrays_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSizes = ReadArrayFromConsole();
+
+            int[,] matrix = new int[matrixSizes[0], matrixSizes[1]];
+
+            int sum = 0;
+
+            for (int i = 0; i < matrixSizes[0]; i++)
+            {
+                int[] input = ReadArrayFromConsole();
+
+                for (int j = 0; j < matrixSizes[1]; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+
+                sum += input.Sum();
+            }
+
+            Console.WriteLine($"{matrixSizes[0]}{Environment.NewLine}{matrixSizes[1]}");
+            Console.WriteLine(sum);
+        }
+
+        private static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(new string[] {" ", ", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
