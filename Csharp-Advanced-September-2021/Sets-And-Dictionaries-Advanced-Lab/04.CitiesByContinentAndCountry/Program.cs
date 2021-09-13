bcfebb0c029ae7a0516a1d06631cd9416bc40ed2
Veronica <veronicagoranova@gmail.com>
2021-09-14 01:24:51 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Sets-And-Dictionaries-Advanced-Lab/04.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continents = 
+                new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (continents.ContainsKey(continent))
+                {
+                    if (continents[continent].ContainsKey(country))
+                    {
+                        continents[continent][country].Add(city);
+                    }
+                    else
+                    {
+                        continents[continent].Add(country, new List<string>() { city });
+                    }
+                }
+                else
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                    continents[continent].Add(country, new List<string>() { city });
+                }
+            }
+
+            foreach (var item in continents)
+            {
+                Console.WriteLine($"{item.Key}:");
+                foreach (var country in item.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(" ", country.Value)}");
+                }
+            }
+        }
+    }
+}
