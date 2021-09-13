d5d07fd5c8c9feac338917197f7600a9c8a659d7
Veronica <veronicagoranova@gmail.com>
2021-09-14 02:19:41 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Sets-And-Dictionaries-Advanced-Lab/06.ParkingLot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parking = new HashSet<string>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] commands = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (commands[0] == "IN")
+                {
+                    parking.Add(commands[1]);
+                }
+                else
+                {
+                    parking.Remove(commands[1]);
+                }
+            }
+
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+            foreach (var item in parking)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
