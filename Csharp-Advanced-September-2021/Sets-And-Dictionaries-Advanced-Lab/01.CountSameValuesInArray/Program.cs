e26591f0ab00c639abb2439dbbca05c752903d6d
Veronica <veronicagoranova@gmail.com>
2021-09-13 14:40:03 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Sets-And-Dictionaries-Advanced-Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sets_And_Dictionaries_Advanced_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> counter = new Dictionary<string, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (counter.ContainsKey(numbers[i]))
+                {
+                    counter[numbers[i]]++;
+                }
+                else
+                {
+                    counter.Add(numbers[i], 1);
+                }
+            }
+
+            foreach (var item in counter)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
