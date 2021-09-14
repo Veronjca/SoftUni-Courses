dacc938844d8b85dc1785dc22d78c27edfaaeff8
Veronica <veronicagoranova@gmail.com>
2021-09-15 00:28:27 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Sets-And-Dictionaries-Advanced-Exercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> table = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    table.Add(elements[j]);
+                }
+            }
+
+            Console.WriteLine($"{String.Join(" ", table)}");
+        }
+    }
+}
