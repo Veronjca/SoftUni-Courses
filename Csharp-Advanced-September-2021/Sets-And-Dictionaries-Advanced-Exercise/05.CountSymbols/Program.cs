c70b305feb52d975670ba4a7a1ae91852f5958b8
Veronica <veronicagoranova@gmail.com>
2021-09-15 01:54:37 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Sets-And-Dictionaries-Advanced-Exercise/05.CountSymbols/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            SortedDictionary<char, int> counter = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+
+                if (counter.ContainsKey(input[i]))
+                {
+                    counter[input[i]]++;
+                }
+                else
+                {
+                    counter.Add(input[i], 1);
+                }
+            }
+
+            foreach (var item in counter)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
