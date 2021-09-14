7879552f77e8b28b62c317a2da2e16f94e6e0a16
Veronica <veronicagoranova@gmail.com>
2021-09-15 00:11:26 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Sets-And-Dictionaries-Advanced-Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sets_And_Dictionaries_Advanced_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                usernames.Add(input);
+            }
+
+            foreach (var item in usernames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
