38f18c64a47c8ddb04cef4c8f7c3d10ecbcad077
Veronica <veronicagoranova@gmail.com>
2021-09-15 01:39:31 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Sets-And-Dictionaries-Advanced-Exercise/04.EvenTimes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, int> counter = new Dictionary<string, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string number = Console.ReadLine();
+
+                if (counter.ContainsKey(number))
+                {
+                    counter[number]++;
+                }
+                else
+                {
+                    counter.Add(number, 1);
+                }
+            }
+
+            foreach (var item in counter)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine($"{item.Key}");
+                }
+            }
+        }
+    }
+}
