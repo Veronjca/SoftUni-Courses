c519ea0259e8429177987dd154627cf59d3ad64c
Veronica <veronicagoranova@gmail.com>
2021-09-21 23:48:51 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Functional-Programming-Lab/04.AddVAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<decimal, decimal> addVat = n => n * 1.2M;
+
+            Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(addVat)
+                .ToList()
+                .ForEach(n => Console.WriteLine($"{n:f2}"));
+        }
+    }
+}
