f8a9bdc5c69a06c7516cc6bef974b231ab074b49
Veronica <veronicagoranova@gmail.com>
2021-09-22 02:08:53 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Functional-Programming-Exercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> minNum = n =>
+            {
+                int min = int.MaxValue;
+                for (int j = 0; j < n.Length; j++)
+                {
+                    if (n[j] < min)
+                    {
+                        min = n[j];
+                    }
+                }
+
+                return min;
+            };
+
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int minValue = minNum(numbers);
+
+            Console.WriteLine(minValue);
+                
+            
+        }
+    }
+}
