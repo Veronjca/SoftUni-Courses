bdb081c8b93a4bf1875fc7d9dd07c9bbb9f60aa5
Veronica <veronicagoranova@gmail.com>
2021-09-21 23:26:26 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Functional-Programming-Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Functional_Programming_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, bool> even = n => n % 2 == 0;
+
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(even)
+                .OrderBy(n => n)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", input));
+        }
+    }
+}
