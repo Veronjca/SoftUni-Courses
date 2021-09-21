6c599c6fc1b32aa5dfb7febbe032a02bd3ddbb0c
Veronica <veronicagoranova@gmail.com>
2021-09-21 23:40:34 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Functional-Programming-Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> uppercase = w => char.IsUpper(w[0]);
+
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(uppercase)
+                .ToList()
+                .ForEach(w => Console.WriteLine(w));
+        }
+    }
+}
