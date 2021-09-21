6bf8da05fe45747680e5e20dd7ac704a2047b373
Veronica <veronicagoranova@gmail.com>
2021-09-22 01:04:23 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Functional-Programming-Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = s => Console.WriteLine($"Sir {s}");
+
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(print);
+        }
+    }
+}
