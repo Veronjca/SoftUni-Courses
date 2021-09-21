0839aa1509df64e7ade719fd2c5c64c7d940425f
Veronica <veronicagoranova@gmail.com>
2021-09-21 23:32:53 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Functional-Programming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine($"{numbers.Length}{Environment.NewLine}{numbers.Sum()}");
+        }
+    }
+}
