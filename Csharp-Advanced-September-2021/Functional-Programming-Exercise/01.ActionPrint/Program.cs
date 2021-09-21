59513cafb24fe3db0fefc298f537e25df2f36459
Veronica <veronicagoranova@gmail.com>
2021-09-22 00:47:55 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Functional-Programming-Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Functional_Programming_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = s => Console.WriteLine(s);
+
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(print);
+        }
+    }
+}
