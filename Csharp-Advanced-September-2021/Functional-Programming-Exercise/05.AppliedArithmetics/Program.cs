8a04c7d2a91d961265ce888d510e417ffc102f06
Veronica <veronicagoranova@gmail.com>
2021-09-22 18:58:42 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Functional-Programming-Exercise/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = ReadArrayFromConsole();
+
+            string command = Console.ReadLine();
+
+            Action<int[]> print = x => Console.WriteLine(String.Join(" ", x));
+
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        input = input.Select(x => x + 1).ToArray();
+                        break;
+                    case "multiply":
+                        input = input.Select(x => x * 2).ToArray();
+                        break;
+                    case "subtract":
+                        input = input.Select(x => x - 1).ToArray();
+                        break;
+                    case "print":
+                        print(input);
+                        break;
+                    default:
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
