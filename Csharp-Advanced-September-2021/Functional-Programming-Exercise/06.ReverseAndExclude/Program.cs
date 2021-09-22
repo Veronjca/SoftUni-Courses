cb3207710f5e3c45ec68336c7f665d5355389ee0
Veronica <veronicagoranova@gmail.com>
2021-09-22 19:59:53 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Functional-Programming-Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int, List<int>> removed = (x, n) =>
+            {
+                List<int> result = new List<int>();
+
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (x[i] % n != 0)
+                    {
+                        result.Add(x[i]);
+                    }
+                }
+                return result;
+            };
+
+            int[] input = Console.ReadLine()
+                         .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                         .Select(int.Parse)
+                         .ToArray()
+                         .Reverse()
+                         .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> result = removed(input, n).ToList();
+
+            Console.WriteLine(String.Join(" ", result));
+            
+
+
+        }
+    }
+}
