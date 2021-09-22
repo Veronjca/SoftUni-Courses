6afbb3836bcbfb3a5cbc184c6b85016b2bba587b
Veronica <veronicagoranova@gmail.com>
2021-09-22 20:30:42 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Functional-Programming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            Predicate<string> filter = w => w.Length <= length;
+
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .FindAll(filter)
+                .ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
