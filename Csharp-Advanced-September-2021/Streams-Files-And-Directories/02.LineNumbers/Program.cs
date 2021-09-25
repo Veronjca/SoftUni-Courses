afba86a45b686e19949527f0b93d59fcc28f8b43
Veronica <veronicagoranova@gmail.com>
2021-09-25 23:40:11 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Streams-Files-And-Directories/02.LineNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader sr = new StreamReader("input.txt");
+            using StreamWriter sw = new StreamWriter("output.txt");
+
+            int counter = 1;
+            while (!sr.EndOfStream)
+            {
+                string currentLine = sr.ReadLine();
+
+                sw.WriteLine($"{counter}. {currentLine}");
+
+                counter++;
+            }
+        }
+    }
+}
