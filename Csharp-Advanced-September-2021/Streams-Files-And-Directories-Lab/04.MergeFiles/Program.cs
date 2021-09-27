031bd1577508833651c474a0c075450d03616348
Veronica <veronicagoranova@gmail.com>
2021-09-28 00:31:15 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Streams-Files-And-Directories-Lab/04.MergeFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _04.MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = File.ReadAllText("input.txt").ToCharArray();
+            char[] input2 = File.ReadAllText("input2.txt").ToCharArray();
+
+            List<string> output = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                output.Add(input[i].ToString());
+                output.Add(input2[i].ToString());
+            }
+            File.WriteAllLines("output.txt", output);
+        }
+    }
+}
