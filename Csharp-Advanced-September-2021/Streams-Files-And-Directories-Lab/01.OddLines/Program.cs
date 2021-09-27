41276784ef8a1dc13e7fe75b91c4953d3e20f21c
Veronica <veronicagoranova@gmail.com>
2021-09-27 23:57:49 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Streams-Files-And-Directories-Lab/01.OddLines/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Streams_Files_And_Directories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader sr = new StreamReader("input.txt");
+            using StreamWriter sw = new StreamWriter("output.txt");
+
+            int counter = 0;
+            while (!sr.EndOfStream)
+            {
+                string currentLine = sr.ReadLine();
+                if (counter % 2 != 0)
+                {
+                    sw.WriteLine(currentLine);
+                }
+                counter++;
+            }
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/Streams-Files-And-Directories-Lab/03.WordCount/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsToSearch = File.ReadAllText("words.txt").Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] whereToSearch = File.ReadAllText("input.txt").Split(new char[] { '-', ' ', ',', '.', '?', '!' });
+              
+                
+            
+            Dictionary<string, int> matches = new Dictionary<string, int>();
+
+           
+            foreach (var item in wordsToSearch)
+            {
+                for (int i = 0; i < whereToSearch.Length; i++)
+                {
+
+                    if (item.ToLower() == whereToSearch[i].ToLower() && matches.ContainsKey(item))
+                    {
+                        matches[item]++;
+                    }
+                    else if (item.ToLower() == whereToSearch[i].ToLower())
+                    {
+                        matches.Add(item, 1);
+                    }
+                }
+
+            }
+
+            List<string> contents = new List<string>();
+            foreach (var item in matches.OrderByDescending(x => x.Value))
+            {
+                contents.Add($"{item.Key}-{item.Value}");
+            }
+
+            File.WriteAllLines("output.txt", contents);
+        }
+
+
+    }
+}
