fffdb583f6a115106a755552f29f2f43129bd4a3
Veronica <veronicagoranova@gmail.com>
2021-09-29 01:30:10 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Streams-Files-And-Directories-Lab/06.FolderSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _06.FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileNames = Directory.GetFiles("TestFolder");
+
+
+            long size = 0;
+            foreach (var item in fileNames)
+            {
+           
+                FileStream stream = new FileStream(item, FileMode.Open);
+                size += stream.Length;
+            }
+
+            Console.WriteLine((decimal)size / 1024 / 1024);
+        }
+    }
+}
