479105eddec29c65900a26e03a000b91400d5ac1
Veronica <veronicagoranova@gmail.com>
2021-09-29 00:51:21 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Streams-Files-And-Directories-Lab/05.SliceFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _05.SliceFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream stream = new FileStream("sliceMe.txt", FileMode.Open);
+
+            byte[]  data = new byte[stream.Length];
+            stream.Read(data);
+
+            int sizePerFile = (int)Math.Ceiling(stream.Length / 4.0);
+            for (int i = 1; i <= 4; i++)
+            {
+                byte[] buffer = new byte[sizePerFile];
+                stream.Read(buffer);
+
+                stream.Read(buffer);
+
+                using FileStream writer = new FileStream($"Part-{i}.txt", FileMode.OpenOrCreate);
+                writer.Write(buffer);
+            }
+            
+        }
+    }
+}
