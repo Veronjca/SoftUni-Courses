4a2a706b5e09ba78e8e4d817591b4a46b5766222
Veronica <veronicagoranova@gmail.com>
2021-09-29 23:45:47 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Streams-Files-And-Directories-Exercise/04.CopyBinaryFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream stream = new FileStream("copyMe.png", FileMode.Open);
+            using FileStream writer = new FileStream("output.png", FileMode.OpenOrCreate);
+
+            byte[] buffer = new byte[256];
+
+            while (true)
+            {
+                int bytesRead = stream.Read(buffer);
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                writer.Write(buffer);
+            }
+        }
+    }
+}
