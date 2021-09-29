b1a0050a4b3501a9c6ddb5d77172972c253b8201
Veronica <veronicagoranova@gmail.com>
2021-09-29 23:14:56 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Streams-Files-And-Directories-Exercise/02.LineNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            string[] text = File.ReadAllLines("text.txt");
+            List<string> temp = new List<string>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int numOfLetters = 0;
+                int numOfPunctuactionMarks = 0;
+
+                for (int j = 0; j < text[i].Length; j++)
+                {
+                    if (char.IsLetter(text[i][j]))
+                    {
+                        numOfLetters++;
+                    }
+                    else if (char.IsPunctuation(text[i][j]))
+                    {
+                        numOfPunctuactionMarks++;
+                    }
+                }                
+                temp.Add($"Line {i+1}: {text[i]}({numOfLetters})({numOfPunctuactionMarks})");
+            }
+            File.WriteAllLines("output.txt", temp);
+        }
+    }
+}
