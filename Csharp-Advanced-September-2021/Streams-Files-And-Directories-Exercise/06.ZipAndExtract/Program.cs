4770a98c87b544e955e160402410f1a8e6b996b4
Veronica <veronicagoranova@gmail.com>
2021-09-30 23:01:52 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Streams-Files-And-Directories-Exercise/06.ZipAndExtract/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO.Compression;
+using System.Text;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ZipFile.CreateFromDirectory(@"C:\VERONICA\zipThat", @"../../../result.zip");
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            StringBuilder fullPath = new StringBuilder(path);
+            fullPath.Append(@"\finalResult");
+
+            ZipFile.ExtractToDirectory(@"../../../result.zip", fullPath.ToString());
+
+            
+        }
+    }
+}
