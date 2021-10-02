9c29aeaeb359efeb5f6d6432eb7e0af0b6beeb9f
Veronica <veronicagoranova@gmail.com>
2021-10-03 00:36:41 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Defining-Classes-Lab/01.Car/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+
+        private string model;
+
+        private int year;
+
+        public string Make 
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model 
+        { 
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year 
+        { 
+            get { return year; }
+            set { year = value; }
+        }
+    }
+}
