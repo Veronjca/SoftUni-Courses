31820284fc9f83f6760fdc4d53ccabeaa0a55ee8
Veronica <veronicagoranova@gmail.com>
2021-10-03 20:48:15 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Defining-Classes-Lab/02.CarExtension/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+
+        private string model;
+
+        private int year;
+
+        private double fuelQuantity;
+
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive (double distance)
+        {
+            bool isEnough = FuelQuantity - distance * FuelConsumption > 0;
+
+            if (isEnough)
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+    
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}
