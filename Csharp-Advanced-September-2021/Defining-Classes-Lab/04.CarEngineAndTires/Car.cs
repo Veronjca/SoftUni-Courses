9e5b4195a75fb24fd55b6be105c8be012d49647c
Veronica <veronicagoranova@gmail.com>
2021-10-04 00:17:24 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Defining-Classes-Lab/04.CarEngineAndTires/Car.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+        public int Year 
+        { 
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double Pressure
+        { 
+            get { return pressure; }
+            set { pressure = value; }
+        }
+    }
+    public class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+
+        public int HorsePower
+        { 
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+
+        public double CubicCapacity
+        {
+            get { return cubicCapacity; }
+            set { cubicCapacity = value; }
+        }
+    }
+    public class Car
+    {
+        private string make;
+
+        private string model;
+
+        private int year;
+
+        private double fuelQuantity;
+
+        private double fuelConsumption;
+
+        private Engine engine;
+
+        private Tire[] tire;
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+        public string Make 
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public Engine Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+
+        public Tire[] Tires
+        {
+            get { return tire; }
+            set { tire = value; }
+        }
+        public void Drive (double distance)
+        {
+            bool isEnough = FuelQuantity - distance * FuelConsumption > 0;
+
+            if (isEnough)
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+    
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/Defining-Classes-Lab/04.CarEngineAndTires/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Tire[] tires = new Tire[4]
+            {
+                new Tire(1, 2.5),
+                new Tire (1, 2.1),
+                new Tire(2, 0.5),
+                new Tire(2, 2.3),
+
+            };
+
+            Engine engine = new Engine(560, 6300);
+
+            Car car = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+        }
+    }
+}
