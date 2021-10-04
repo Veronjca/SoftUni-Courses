0a0cade1a12af23b77b4acda24b233608729bbf7
Veronica <veronicagoranova@gmail.com>
2021-10-04 20:14:24 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Defining-Classes-Exercise/02.CreatingConstructors/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+
+        private int age;
+
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age)
+            :this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name
+        { 
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
