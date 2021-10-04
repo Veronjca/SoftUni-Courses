443d5a0f5cd754425d351e7a883e957c8142cbc7
Veronica <veronicagoranova@gmail.com>
2021-10-04 22:17:03 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Defining-Classes-Exercise/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            Members = new List<Person>();
+        }
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person member)
+        {
+            this.Members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            int maxAge = Members.Max(x => x.Age);
+            return Members.First(member => member.Age == maxAge);
+
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/Defining-Classes-Exercise/03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family members = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = info[0];
+                int age = int.Parse(info[1]);
+                members.AddMember(new Person(name, age));
+            }
+
+            Console.WriteLine($"{members.GetOldestMember().Name} {members.GetOldestMember().Age}");
+        }
+    }
+}
