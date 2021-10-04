f22a5daae38e3e55a0f56c9e38b053814e08af29
Veronica <veronicagoranova@gmail.com>
2021-10-05 00:07:44 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Defining-Classes-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class DateModifier
+    {
+        public DateModifier(string firstDate, string secondDate)
+        {
+            FirstDate = firstDate;
+            SecondDate = secondDate;
+        }
+        public string FirstDate { get; set; }
+        public string SecondDate { get; set; }
+
+        public double Difference(string firstDate, string secondDate)
+        {
+            double days = 0;
+            DateTime startDate = Convert.ToDateTime(firstDate);
+            DateTime endDate = Convert.ToDateTime(secondDate);
+
+            return days = Math.Abs((endDate - startDate).TotalDays);
+
+
+        }
+        
+    }
+}
---
+++ Csharp-Advanced-September-2021/Defining-Classes-Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstDate;
+            string secondDate;
+
+            DateModifier dates = new DateModifier(secondDate = Console.ReadLine(), firstDate = Console.ReadLine());
+
+            double diff = dates.Difference(dates.FirstDate, dates.SecondDate);
+
+            Console.WriteLine(diff);
+
+        }
+    }
+}
