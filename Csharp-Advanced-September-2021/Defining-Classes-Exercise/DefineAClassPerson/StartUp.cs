37bba124e69e2e23b36a2ee0349374eab5a83395
Veronica <veronicagoranova@gmail.com>
2021-10-04 20:08:56 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Defining-Classes-Exercise/DefineAClassPerson/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person first = new Person();
+            first.Name = "Peter";
+            first.Age = 20;
+
+            Person second = new Person 
+            { 
+                Name = "George",
+                Age = 18
+            };
+
+            Person third = new Person
+            {
+                Name = "Jose",
+                Age = 43
+            };
+        }
+    }
+}
