06e2e1db8cc30376f006888337bffa67e680a6c9
Veronica <veronicagoranova@gmail.com>
2021-10-04 22:28:39 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Defining-Classes-Exercise/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = info[0];
+                int age = int.Parse(info[1]);
+                people.Add(new Person(name, age));
+            }
+
+            people.Where(p => p.Age > 30).OrderBy(p => p.Name).ToList().ForEach(p => Console.WriteLine($"{p.Name} - {p.Age}"));
+        }
+    }
+}
