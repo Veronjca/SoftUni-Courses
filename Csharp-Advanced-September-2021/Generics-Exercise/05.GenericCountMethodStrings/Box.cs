3b725397dd80e4c588fc4854da8fc9158e95687a
Veronica <veronicagoranova@gmail.com>
2021-10-10 01:10:48 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Generics-Exercise/05.GenericCountMethodStrings/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.GenericCountMethodStrings
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/Generics-Exercise/05.GenericCountMethodStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.GenericCountMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<string>> list = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string current = Console.ReadLine();
+                var currentBox = new Box<string>(current);
+                list.Add(currentBox);
+            }
+
+            string compareWord = Console.ReadLine();
+            Console.WriteLine(Count(list, compareWord)); 
+            
+        }        
+
+        public static int Count<T>(List<Box<T>> list, T word)
+            where T: IComparable
+        {
+            list = list.Where(x => x.Value.CompareTo(word) > 0).ToList();
+
+           
+            return list.Count;
+        }
+    }
+}
