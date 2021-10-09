76585e70691a084651b3b24e91d8582093e0156c
Veronica <veronicagoranova@gmail.com>
2021-10-09 23:34:04 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Generics-Lab/03.GenericScale/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        public EqualityScale(T firstElement, T secondElement)
+        {
+            FirstElement = firstElement;
+            SecondElement = secondElement;
+        }
+        public T FirstElement{ get; set; }
+        public T SecondElement { get; set; }
+
+        public bool AreEqual()
+        {
+            return FirstElement.Equals(SecondElement);
+        }
+
+    }
+}
---
+++ Csharp-Advanced-September-2021/Generics-Lab/03.GenericScale/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
