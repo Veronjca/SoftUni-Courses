6a8365636c8cb8027a1dd39482b5dae97a980139
Veronica <veronicagoranova@gmail.com>
2021-10-10 00:33:47 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Generics-Exercise/04.GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.GenericSwapMethodIntegers
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/Generics-Exercise/04.GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.GenericSwapMethodIntegers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<Box<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                var currentBox = new Box<int>(number);
+                list.Add(currentBox);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Swap(list, indexes);
+            list.ForEach(x => Console.WriteLine(x));
+        }
+
+        public static void Swap<T>(List<T> input, int[] indexes)
+        {
+            T firstString = input[indexes[0]];
+            T secondString = input[indexes[1]];
+
+            input[indexes[0]] = secondString;
+            input[indexes[1]] = firstString;
+        }
+    }
+}
