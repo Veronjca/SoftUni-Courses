aa914980a5fd975916043fb6a579c5baacbc6a1f
Veronica <veronicagoranova@gmail.com>
2021-10-10 00:07:10 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Generics-Exercise/02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.GenericBoxOfInteger
+{
+    public class Box <T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/Generics-Exercise/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.GenericBoxOfInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                var cuurentBox = new Box<int>(number);
+                Console.WriteLine(cuurentBox);
+            }
+        }
+    }
+}
