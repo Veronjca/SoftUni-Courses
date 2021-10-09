a29a5681b7378835673d4a520a851d5dbb576ee4
Veronica <veronicagoranova@gmail.com>
2021-10-10 00:28:03 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Generics-Exercise/03.GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.GenericSwapMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<string>> list = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentString = Console.ReadLine();
+                Box<string> currentBox = new Box<string>(currentString);
+                list.Add(currentBox);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Swap(list, indexes);
+            list.ForEach(x => Console.WriteLine(x));
+
+        }
+
+        public static void Swap<T>(List<T> input, int[] indexes)
+        {
+            T firstString = input[indexes[0]];
+            T secondString = input[indexes[1]];
+
+            input[indexes[0]] = secondString;
+            input[indexes[1]] = firstString;
+        }
+    }
+}
