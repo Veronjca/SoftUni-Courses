fe1d80d7f8eb5959cded0d9fe8b1dce62fa16b09
Veronica <veronicagoranova@gmail.com>
2021-10-09 23:23:13 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Generics-Lab/02.GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T element)
+        {
+            T[] array = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = element;
+            }
+            return array;
+        }
+
+    }
+}
