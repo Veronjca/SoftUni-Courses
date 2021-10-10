d15d8f6e683294463134c83953a89f805e83d1fd
Veronica <veronicagoranova@gmail.com>
2021-10-10 16:56:07 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Generics-Exercise/08.Threeuple/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _08.ThreeUple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (i == 0)
+                {
+                    string town = String.Empty;
+
+                    try
+                    {
+                        town = $"{input[3]} {input[4]}";
+                    }
+                    catch (Exception)
+                    {
+
+                        town = input[3];
+                    }
+                    string name = $"{input[0]} {input[1]}";
+                    string address = input[2];
+
+                    var current = new Threeuple<string, string, string>(name, address, town);
+                    Console.WriteLine(current);
+                }
+                else if (i == 1)
+                {
+                    string name = input[0];
+                    string beerLiters = input[1];
+                    bool drunk = input[2] == "drunk";
+                    var current = new Threeuple<string, string, bool>(name, beerLiters, drunk);
+                    Console.WriteLine(current);
+                }
+                else
+                {
+                    string name = input[0];
+                    double bankBalance = double.Parse(input[1]);
+                    string bankName = input[2];
+                    var current = new Threeuple<string, double, string>(name, bankBalance, bankName);
+                    Console.WriteLine(current);
+
+                }
+            }
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/Generics-Exercise/08.Threeuple/Threeuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.ThreeUple
+{
+    public class Threeuple<T, T1, T2>
+    {
+        public Threeuple(T firstItem, T1 secondItem, T2 thirdItem)
+        {
+            FirstItem = firstItem;
+            SecondItem = secondItem;
+            ThirdItem = thirdItem;
+        }
+     
+        public T FirstItem { get; set; }
+        public T1 SecondItem { get; set; }
+        public T2 ThirdItem { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem} -> {ThirdItem}";
+        }
+    }
+}
