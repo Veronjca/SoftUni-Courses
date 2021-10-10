634c1f05a1528dc4be9f5f8429ef001d252c2e6d
Veronica <veronicagoranova@gmail.com>
2021-10-10 15:56:38 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Generics-Exercise/06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.GenericCountMethodDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<double>> list = new List<Box<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double current = double.Parse(Console.ReadLine());
+                var currentBox = new Box<double>(current);
+                list.Add(currentBox);
+            }
+
+            double compareWord = double.Parse(Console.ReadLine());
+            Console.WriteLine(Count(list, compareWord));
+
+        }
+
+        public static int Count<T>(List<Box<T>> list, T word)
+            where T : IComparable
+        {
+            list = list.Where(x => x.Value.CompareTo(word) > 0).ToList();
+
+            return list.Count;
+        }
+    }
+}
+
