a8e7f89e63241f08e30cfb16c37906d1c6e235a3
Veronica <veronicagoranova@gmail.com>
2021-10-10 17:14:55 +0300

    update

--- Csharp-Advanced-September-2021/Generics-Exercise/09.CustomLinkedList/DoublyLinkedList.cs
+++ Csharp-Advanced-September-2021/Generics-Exercise/09.CustomLinkedList/DoublyLinkedList.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CustomDoublyLinkedList
+namespace CustomLinkedList
 {
     public class CustomDoublyLinkedList<T>
     {
--- Csharp-Advanced-September-2021/Generics-Exercise/09.CustomLinkedList/LinkedListItem.cs
+++ Csharp-Advanced-September-2021/Generics-Exercise/09.CustomLinkedList/LinkedListItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CustomDoublyLinkedList
+namespace CustomLinkedList
 {
     public class LinkedListItem<T>
     {
