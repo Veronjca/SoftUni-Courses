5c67d365e8c62e5bad04ba5b43dfb967a90dcf88
Veronica <veronicagoranova@gmail.com>
2021-10-10 16:30:30 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Generics-Exercise/07.Tuple/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+               
+                if (i == 0)
+                {                   
+                    var current = new Tuple<string, string>($"{input[0]} {input[1]}", input[2]);
+                    Console.WriteLine(current);
+                }
+                else if(i == 1)
+                {
+                    var current = new Tuple<string, int>(input[0] , int.Parse(input[1]));
+                    Console.WriteLine(current);
+                }
+                else
+                {
+                   var current = new Tuple<int, double>(int.Parse(input[0]), double.Parse(input[1]));
+                    Console.WriteLine(current);
+                }               
+            }
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/Generics-Exercise/07.Tuple/Tuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    public class Tuple<T, T1>
+    {
+
+        public Tuple(T firstItem, T1 secondItem)
+        {
+            FirstItem = firstItem;
+            SecondItem = secondItem;
+        }
+        public T FirstItem { get; set; }
+        public T1 SecondItem { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem}";
+        }
+
+    }
+}
