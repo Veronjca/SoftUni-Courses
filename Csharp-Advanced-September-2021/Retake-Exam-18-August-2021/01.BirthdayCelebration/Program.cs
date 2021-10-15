332be6bb3b8dd704dcf31dc44aca3ba2c6f928a9
Veronica <veronicagoranova@gmail.com>
2021-10-16 02:52:44 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Retake-Exam-18-August-2021/01.BirthdayCelebration/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BirthdayCelebration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> guestsCapacity = new Stack<int>(ReadArrayFromConsole().Reverse());
+            Stack<int> plates = new Stack<int>(ReadArrayFromConsole());
+
+            int wastedFood = 0;
+
+            while (guestsCapacity.Count > 0 && plates.Count > 0)
+            {
+
+                int currentCup = guestsCapacity.Peek();
+                int currentBottle = plates.Pop();
+
+                if (currentBottle - currentCup > 0)
+                {
+                    guestsCapacity.Pop();
+                    wastedFood += currentBottle - currentCup;
+                }
+                else if (currentBottle - currentCup < 0)
+                {
+                    guestsCapacity.Pop();
+                    guestsCapacity.Push(Math.Abs(currentBottle - currentCup));
+
+                }
+                else
+                {
+                    guestsCapacity.Pop();
+                }
+            }
+
+            if (guestsCapacity.Count > 0)
+            {
+                Console.WriteLine($"Guests: {String.Join(" ", guestsCapacity)}");
+            }
+            else
+            {
+
+                Console.WriteLine($"Plates: {String.Join(" ", plates)}");
+            }
+
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+
+        private static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+    }
+
