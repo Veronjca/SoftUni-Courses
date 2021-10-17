5edb817b5b4dd13e69b7d47b0c013721b9be3cbc
Veronica <veronicagoranova@gmail.com>
2021-10-17 03:01:02 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Iterators-And-Comparators-Exercise/03.Stack/CustomStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        public List<T> Elements { get; set; } = new List<T>();
+
+        public void Push(T element)
+        {
+            Elements.Insert(0, element);
+        }
+
+        public void Pop()
+        {
+            if (Elements.Count == 0)
+            {
+                Console.WriteLine("No elements");
+                return;
+            }
+            Elements.RemoveAt(0);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Elements.Count; i++)
+            {
+                yield return Elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+       
+    }
+}
---
+++ Csharp-Advanced-September-2021/Iterators-And-Comparators-Exercise/03.Stack/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _03.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+
+            CustomStack<string> stack = new CustomStack<string>();
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] commandArgs = command.Split(new string[] {" ", ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (commandArgs[0] == "Push")
+                {
+                    for (int i = 1; i < commandArgs.Length; i++)
+                    {
+                        stack.Push(commandArgs[i]);
+                    }
+                }
+                else 
+                {
+                    stack.Pop();
+                }
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
