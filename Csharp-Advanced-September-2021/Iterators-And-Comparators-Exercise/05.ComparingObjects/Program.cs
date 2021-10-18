0317890d65cc3779cb8dcf617b7f128524adc7e2
Veronica <veronicagoranova@gmail.com>
2021-10-19 01:02:10 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Iterators-And-Comparators-Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> peoples = new List<Person>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] inputArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = inputArgs[0];
+                int age =int.Parse(inputArgs[1]);
+                string town = inputArgs[2];
+
+                peoples.Add(new Person(name, age, town));
+            }
+
+            int n = int.Parse(Console.ReadLine());
+            int countOfMatches = 0;
+            int countOfNotEqual = 0;
+            Person personToCompare = peoples[n-1];
+
+            if (peoples.Count > n)
+            {
+                foreach (var person in peoples)
+                {
+                    int result = personToCompare.CompareTo(person);
+                    if (result == 0)
+                    {
+                        countOfMatches++;
+                    }
+                    else
+                    {
+                        countOfNotEqual++;
+                    }
+                }
+            }         
+            
+            Console.WriteLine(countOfMatches > 0 ? $"{countOfMatches} {countOfNotEqual} {peoples.Count}" : "No matches");
+        }
+    }
+}
