6848b744dfcb358437ac43fa90fd89a11be22b7c
Veronica <veronicagoranova@gmail.com>
2021-10-18 03:35:12 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Iterators-And-Comparators-Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        public Lake(params int[] numbers)
+        {
+            this.StoneNumbers = numbers.ToList();
+        }
+        public List<int> StoneNumbers { get; set; }
+
+        public List<int> Rearrange()
+        {
+            List<int> temp = new List<int>();
+            for (int i = 0; i < this.StoneNumbers.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    temp.Add(this.StoneNumbers[i]);
+                }             
+            }
+
+            for (int i = this.StoneNumbers.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    temp.Add(this.StoneNumbers[i]);
+                }
+            }
+
+            return temp;
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new Iterator(Rearrange());
+        }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private class Iterator : IEnumerator<int>
+        {
+            private int index;
+            public Iterator(List<int> numbers)
+            {
+                this.Reset();
+                this.StoneNumbers = numbers;
+            }
+            public List<int> StoneNumbers { get; private set; }
+
+            public int Current => this.StoneNumbers[index];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                return ++this.index < this.StoneNumbers.Count;
+            }
+
+            public void Reset()
+            {
+                this.index = -1;
+            }
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/Iterators-And-Comparators-Exercise/04.Froggy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Lake lake = new Lake(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            lake.Rearrange();
+
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
