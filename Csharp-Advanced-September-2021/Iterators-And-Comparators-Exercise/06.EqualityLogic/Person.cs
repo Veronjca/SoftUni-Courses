f02417ac22b022f1c185b02b77abfa2f36da5b9b
Veronica <veronicagoranova@gmail.com>
2021-10-19 22:11:02 +0300

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/Iterators-And-Comparators-Exercise/06.EqualityLogic/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _06.EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name.CompareTo(other.Name) != 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+            if (this.Age.CompareTo(other.Age) != 0)
+            {
+                return this.Age.CompareTo(other.Age);
+            }
+
+            return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person other = obj as Person;
+            if (other != null)
+            {
+                return this.Name == other.Name && this.Age == other.Age;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Age);
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/Iterators-And-Comparators-Exercise/06.EqualityLogic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.EqualityLogic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Person> hashset = new HashSet<Person>();
+            SortedSet<Person> sorted = new SortedSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                hashset.Add(new Person(input[0], int.Parse(input[1])));
+                sorted.Add(new Person(input[0], int.Parse(input[1])));
+            }
+
+            Console.WriteLine($"{hashset.Count}\n{sorted.Count}");
+        }
+    }
+}
