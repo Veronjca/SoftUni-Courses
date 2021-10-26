d009d890fe3698afc43a292f9610e75c04157d25
Veronica <veronicagoranova@gmail.com>
2021-10-27 00:59:08 +0300

    Adding solved problem.

---
+++ Csharp-OOP/Inheritance-Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random = new Random();
+        public string RandomString()
+        {
+            int index = random.Next(0, this.Count);
+            return this[index];
+        }
+        public void Remove()
+        {
+            int index = random.Next(0, this.Count);
+            this.RemoveAt(index);
+        }
+    }
+}
