75d67bbeb735522b8092c33b67318a4647602234
Veronica <veronicagoranova@gmail.com>
2021-10-27 01:10:02 +0300

    Adding solved problem.

---
+++ Csharp-OOP/Inheritance-Lab/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(new List<string> { "abc", "bbc", "aaa" });
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
