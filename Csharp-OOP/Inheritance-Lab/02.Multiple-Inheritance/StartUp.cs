4f380244f00dc476cbcca9fb69bbe5f125b8b8e0
Veronica <veronicagoranova@gmail.com>
2021-10-27 00:32:46 +0300

    Adding solved problem.

---
+++ Csharp-OOP/Inheritance-Lab/02.Multiple-Inheritance/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Farm
+{
+   public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
