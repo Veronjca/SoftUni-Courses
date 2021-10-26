57053a89b79c19e3b4c5d760847cb24ededa5d48
Veronica <veronicagoranova@gmail.com>
2021-10-27 00:14:57 +0300

    Adding solved problem.

---
+++ Csharp-OOP/Inheritance-Lab/01.Single-Inheritance/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Farm
+{
+   public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+        }
+    }
+}
