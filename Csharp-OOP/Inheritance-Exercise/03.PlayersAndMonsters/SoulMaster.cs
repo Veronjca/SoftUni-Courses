14b03b767e48548347fe01564e137222d51b0256
Veronica <veronicagoranova@gmail.com>
2021-10-28 01:18:34 +0300

    Adding solved problem.

---
+++ Csharp-OOP/Inheritance-Exercise/03.PlayersAndMonsters/SoulMaster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class SoulMaster : DarkWizard
+    {
+        public SoulMaster(string username, int level)
+            : base(username, level)
+        {
+
+        }
+    }
+}
