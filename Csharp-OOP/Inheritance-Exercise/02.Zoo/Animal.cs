f5c697221ca97041aa53a093b8923425f10d21b0
Veronica <veronicagoranova@gmail.com>
2021-10-28 01:04:08 +0300

    Adding solved problem.

---
+++ Csharp-OOP/Inheritance-Exercise/02.Zoo/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
