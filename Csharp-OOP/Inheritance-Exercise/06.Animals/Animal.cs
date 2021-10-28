321850c0197b5540439abf0fb17aa157a9cda7ab
Veronica <veronicagoranova@gmail.com>
2021-10-29 00:29:43 +0300

    Adding solved problem.

---
+++ Csharp-OOP/Inheritance-Exercise/06.Animals/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public virtual string ProduceSound()
+        {
+            return "";
+        }
+    }
+}
---
+++ Csharp-OOP/Inheritance-Exercise/06.Animals/StartUp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string animalType; 
+
+            while ((animalType = Console.ReadLine()) != "Beast!")
+            {
+                string[] arguments = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = arguments[0];
+                int age = int.Parse(arguments[1]);
+                string gender = arguments[2];
+
+                switch (animalType)
+                {
+                    case "Dog":
+                        {
+                            Dog dog = new Dog(name, age, gender);
+                            animals.Add(dog);
+                            break;
+                        }
+
+                    case "Cat":
+                        {
+                            Cat cat = new Cat(name, age, gender);
+                            animals.Add(cat);
+                            break;
+                        }
+
+                    case "Kitten":
+                        {
+                            Kitten kitten = new Kitten(name, age);
+                            animals.Add(kitten);
+                            break;
+                        }
+
+                    case "Frog":
+                        {
+                            Frog frog = new Frog(name, age, gender);
+                            animals.Add(frog);
+                            break;
+                        }
+
+                    case "Tomcat":
+                        {
+                            Tomcat tomcat = new Tomcat(name, age);
+                            animals.Add(tomcat);
+                            break;
+                        }
+                }
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.GetType().Name);
+                Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                Console.WriteLine(animal.ProduceSound());
+            }
+        }
+    }
+}
