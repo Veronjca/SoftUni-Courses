4c27ade58d9161d628f74964bec7236a8b6cb5d6
Veronica <veronicagoranova@gmail.com>
2021-10-31 01:41:51 +0300

    Adding solved problem.

---
+++ Csharp-OOP/Encapsulation-Lab/01.Persons/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+        public int Age { get; private set; }
+        public string FirstName { get; private set; }
+        public string  LastName { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+    }
+}
---
+++ Csharp-OOP/Encapsulation-Lab/01.Persons/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                Person currentPerson = new Person(firstName, lastName, age);
+                people.Add(currentPerson);
+            }
+
+           people.OrderBy(x => x.FirstName).ThenBy(x => x.Age).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
