cfdeac862a815295f9a9948006e30c5ff8fba447
Veronica <veronicagoranova@gmail.com>
2021-10-31 02:11:09 +0300

    Adding solved problem.

---
+++ Csharp-OOP/Encapsulation-Lab/03.Validation/Person.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+        public int Age
+        {
+            get => this.age;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                this.age = value;
+            }
+        }
+        public string FirstName
+        {
+            get => this.firstName;
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                this.firstName = value;
+            }
+        }
+        public string  LastName
+        {
+            get => this.lastName;
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                this.lastName = value;
+            }
+        }
+        public decimal Salary 
+        {
+            get => this.salary;
+            set
+            {
+                if (value < 460M)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                this.salary = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                this.Salary += this.Salary * ((percentage / 100)/ 2);
+            }
+            else
+            {
+                this.Salary += this.Salary * (percentage / 100);
+            }
+        }
+    }
+}
