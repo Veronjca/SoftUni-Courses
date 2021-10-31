ab1ebc4cc15f5b1dd0d441d1a7b535f987dacef3
Veronica <veronicagoranova@gmail.com>
2021-11-01 00:34:37 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Encapsulation-Exercise/02.AnimalFarm/Models/Chicken.cs
@@ -0,0 +1,85 @@
+﻿namespace AnimalFarm.Models
+{
+    public class Chicken
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
+
+        private string name;
+        private int age;
+
+        public Chicken(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            internal set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    System.Console.WriteLine("Name cannot be empty.");
+                    return;
+                }
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            protected set
+            {
+                if (value < MinAge || value > MaxAge)
+                {
+                    System.Console.WriteLine("Age should be between 0 and 15.");
+                    return;
+                }
+                this.age = value;
+            }
+        }
+
+        public double ProductPerDay
+        {
+			get
+			{				
+				return this.CalculateProductPerDay();
+			}
+        }
+
+        private double CalculateProductPerDay()
+        {
+            switch (this.Age)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return 1.5;
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    return 2;
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
+        }
+    }
+}
---
+++ Csharp-OOP/Encapsulation-Exercise/02.AnimalFarm/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AnimalFarm
+{
+    using System;
+    using AnimalFarm.Models;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Chicken chicken = new Chicken(name, age);
+            if (chicken.Age != 0 && !string.IsNullOrEmpty(chicken.Name) && !string.IsNullOrWhiteSpace(chicken.Name))
+            {
+                Console.WriteLine(
+                "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                chicken.Name,
+                chicken.Age,
+                chicken.ProductPerDay);
+            }
+            
+        }
+    }
+}
