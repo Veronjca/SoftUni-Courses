a5b5e572102e0879a1ad6625f786354b1202e803
Veronica <veronicagoranova@gmail.com>
2021-10-31 19:53:27 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Encapsulation-Lab/04.Team/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                decimal salary = decimal.Parse(input[3]);
+                Person currentPerson = new Person(firstName, lastName, age, salary);
+                people.Add(currentPerson);
+            }
+
+            Team team = new Team("Levski");
+            foreach (var person in people)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"{team.FirstTeam.Count}{Environment.NewLine}{team.ReserveTeam.Count}");
+        }
+    }
+}
---
+++ Csharp-OOP/Encapsulation-Lab/04.Team/Team.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam = new List<Person>();
+        private List<Person> reserveTeam = new List<Person>();
+        public Team(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name
+        { 
+            get => this.name;
+            set => this.name = value;
+        }
+        public IReadOnlyCollection<Person> FirstTeam 
+        {
+            get => this.firstTeam.AsReadOnly();
+        } 
+        public IReadOnlyCollection<Person> ReserveTeam 
+        {
+            get => this.reserveTeam.AsReadOnly();
+        } 
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age < 40)
+            {
+                this.firstTeam.Add(player);
+            }
+            else
+            {
+                this.reserveTeam.Add(player);
+            }
+        }
+    }
+}
