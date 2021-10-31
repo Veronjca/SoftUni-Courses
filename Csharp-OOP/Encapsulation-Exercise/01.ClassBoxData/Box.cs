c238820db7e2e31dc850806dcd095ffe3732aa8c
Veronica <veronicagoranova@gmail.com>
2021-11-01 00:07:13 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Encapsulation-Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length 
+        { 
+            get => length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Length cannot be zero or negative.");
+                    return;
+                }
+                length = value;
+            }
+        }
+        public double Width 
+        { 
+            get => width; 
+            private set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Width cannot be zero or negative.");
+                    return;
+                }
+                width = value;
+            }
+        }
+        public double Height 
+        { 
+            get => height; 
+            private set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Height cannot be zero or negative.");
+                    return;
+                }
+                height = value;
+            }
+        }
+
+        public double CalculateSurfaceArea()
+        {
+            return (2 * this.Length * this.Width) + (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+        }
+
+        public double CalculateLateralSurfaceArea()
+        {
+            return (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+        }
+
+        public double Volume()
+        {
+            return this.Length * this.Width * this.Height;
+        }
+    }
+}
---
+++ Csharp-OOP/Encapsulation-Exercise/01.ClassBoxData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+
+            if (box.Length > 0 && box.Width > 0 && box.Height > 0)
+            {
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            
+        }
+    }
+}
