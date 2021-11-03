58e4b9eafb7e50220d53a5350cd704ec028e0037
Veronica <veronicagoranova@gmail.com>
2021-11-03 23:50:45 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Interfaces-And-Abstraction-Exercise/02.MultipleImplementation/Citizen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthDate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthDate;
+        }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
