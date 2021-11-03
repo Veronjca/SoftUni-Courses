a30f94789ecd8cdf99641b556aa8d67afe8d25f4
Veronica <veronicagoranova@gmail.com>
2021-11-03 23:42:56 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Interfaces-And-Abstraction-Exercise/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
