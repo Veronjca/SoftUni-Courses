013887effbb89e9a188ec6aece748a2b0d4db526
Veronica <veronicagoranova@gmail.com>
2021-11-03 02:05:06 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Interfaces-And-Abstraction-Lab/01.Shapes/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.Radius = radius;
+        }
+        public int Radius { get; private set; }
+        public void Draw()
+        {
+            double @in = this.Radius - 0.4;
+            double @out = this.Radius + 0.4;
+            for (double y = this.Radius; y >= -this.Radius; --y)
+            {
+                for (double x = -this.Radius; x < @out; x+= 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= @in * @in && value <= @out * @out)
+                    {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Csharp-OOP/Interfaces-And-Abstraction-Lab/01.Shapes/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public void Draw()
+        {
+            DrawLine(this.Width, '*', '*');
+            for (int i = 1; i < this.Height - 1; ++i)
+            {
+                DrawLine(this.Width, '*', ' ');
+            }
+            DrawLine(this.Width, '*', '*');
+        }
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width-1; ++i)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
