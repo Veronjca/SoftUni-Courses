35484f7d80ccb21a3863ff7ee7f8578662e93ced
Veronica <veronicagoranova@gmail.com>
2021-11-03 20:00:26 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Interfaces-And-Abstraction-Lab/02.Cars/Seat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "";
+        }
+
+        public string Stop()
+        {
+            return "";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Seat {this.Model}";
+        }
+    }
+}
---
+++ Csharp-OOP/Interfaces-And-Abstraction-Lab/02.Cars/Tesla.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public string Start()
+        {
+            return "";
+        }
+
+        public string Stop()
+        {
+            return "";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with battery {this.Battery}";
+        }
+    }
+}
