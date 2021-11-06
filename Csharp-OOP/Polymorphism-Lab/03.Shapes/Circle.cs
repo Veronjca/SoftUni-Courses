1cfe330babf7892f3f2c4071ae8a24945c6df659
Veronica <veronicagoranova@gmail.com>
2021-11-07 00:08:20 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Polymorphism-Lab/03.Shapes/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public double Radius { get; set; }
+ 
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(this.Radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Math.PI * 2 * this.Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
---
+++ Csharp-OOP/Polymorphism-Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height { get; set; }
+
+        public double Width { get; set; }
+
+        public override double CalculateArea()
+        {
+            return this.Height * this.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return  this.Width * 2 + this.Height * 2;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
