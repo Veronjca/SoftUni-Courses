dc468033ff6e8b74a0ec795f845809f07c597fa6
Veronica <veronicagoranova@gmail.com>
2021-11-11 00:47:32 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Exception-Handling-Exercise/05.ConvertToDouble/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05.ConvertToDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = 
+                {
+                "-1,035.77219", 
+                "blqks", 
+                "6.34",
+                "100",
+                "-21.5",
+                "1", 
+                "muhahah",
+                "1.29e325",
+            };
+            double result;
+
+            foreach (string value in values)
+            {
+                try
+                {
+                    result = Convert.ToDouble(value);
+                    if (double.IsPositiveInfinity(result) || double.IsNegativeInfinity(result))
+                    {
+                        throw new OverflowException();
+                    }
+                    Console.WriteLine($"Converted '{value}' to {result}.");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Unable to convert '{value}' to a Double.");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"'{value}' is outside the range of a Double.");
+                }
+            }
+        }
+    }
+}
