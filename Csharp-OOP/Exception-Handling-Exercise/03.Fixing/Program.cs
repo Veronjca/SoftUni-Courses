4086d4200596e32d0a818152e6f807c9e6c51c89
Veronica <veronicagoranova@gmail.com>
2021-11-11 00:06:22 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Exception-Handling-Exercise/03.Fixing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03.Fixing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekdays = new string[5];
+            weekdays[0] = "Sunday";
+            weekdays[1] = "Monday";
+            weekdays[2] = "Tuesday";
+            weekdays[3] = "Wednesday";
+            weekdays[4] = "Thursday";
+
+                for (int i = 0; i <= 5; i++)
+                {
+                    try
+                    {
+                        Console.WriteLine(weekdays[i].ToString());
+                    }
+                    catch (IndexOutOfRangeException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                Console.ReadLine();
+
+        }
+    }
+}
