aeabd80c4a7c885367a4f8fc9b6de88517cb536f
Veronica <veronicagoranova@gmail.com>
2021-11-10 23:08:52 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Exception-Handling-Exercise/01.SquareRoot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            try
+            {
+                ValidateNumber(number);
+                double root = Math.Sqrt(int.Parse(number));
+                Console.WriteLine(root);
+
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+
+        }
+
+        private static void ValidateNumber(string number)
+        {
+            if (int.Parse(number) < 0)
+            {
+                throw new ArgumentException();
+            }
+            if (!int.TryParse(number, out _))
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
