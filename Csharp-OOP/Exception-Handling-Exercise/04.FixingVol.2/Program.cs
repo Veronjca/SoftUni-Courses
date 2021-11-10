788341081139275c9390e661b89c0fe30d3ad901
Veronica <veronicagoranova@gmail.com>
2021-11-11 00:12:59 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Exception-Handling-Exercise/04.FixingVol.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03.FixingVol._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            byte result;
+
+            firstNumber = 30;
+            secondNumber = 60;
+            
+            try
+            {
+                result = Convert.ToByte(firstNumber * secondNumber);
+                Console.WriteLine($"{firstNumber} x {secondNumber} = {result}");
+            }
+            catch (OverflowException ex )
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
