b1912bbb89183d49f0ccce32dc2ab8722c2bfda8
Veronica <veronicagoranova@gmail.com>
2021-11-18 01:02:48 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Reflection-And-Attributes-Lab/01.Stealer/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string []  fieldsToInvestigate)
+        {
+            Hacker hacker = new Hacker();
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Class under investigation: {className}");
+
+            foreach (var field in hacker.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static))
+            {
+                if (fieldsToInvestigate.ToList().Contains(field.Name))
+                {
+                    result.AppendLine($"{field.Name} = {field.GetValue(hacker)}");
+                }
+                
+            }
+
+            return result.ToString().TrimEnd();
+
+        }
+    }
+}
