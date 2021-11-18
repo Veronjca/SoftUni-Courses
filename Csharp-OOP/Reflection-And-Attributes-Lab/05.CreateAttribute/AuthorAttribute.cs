07d94db9745505b156c2654fe893d286afc527f1
Veronica <veronicagoranova@gmail.com>
2021-11-18 23:59:16 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Reflection-And-Attributes-Lab/05.CreateAttribute/AuthorAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ Csharp-OOP/Reflection-And-Attributes-Lab/05.CreateAttribute/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
