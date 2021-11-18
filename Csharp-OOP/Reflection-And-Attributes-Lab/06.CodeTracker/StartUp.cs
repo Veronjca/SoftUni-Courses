a6b6c6d52f5a4e242646eca29d10f8d83c5a9157
Veronica <veronicagoranova@gmail.com>
2021-11-19 00:31:41 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Reflection-And-Attributes-Lab/06.CodeTracker/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Viki")]
+    public class StartUp
+    {
+        [Author("Pavlin")]
+        [Author("Emre")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Csharp-OOP/Reflection-And-Attributes-Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            var type = Type.GetType("StartUp");
+            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+
+            StringBuilder result = new StringBuilder();
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+
+                }
+
+            }
+        }
+    }
+}
