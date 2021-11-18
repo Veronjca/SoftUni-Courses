ef3026dda88520b3547a3aa89f7f188c114a2143
Veronica <veronicagoranova@gmail.com>
2021-11-18 02:14:22 +0200

    Adding solved problem.

---
+++ Csharp-OOP/Reflection-And-Attributes-Lab/03.MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        private StringBuilder result = new StringBuilder();
+
+        public string RevealPrivateMethods(string className)
+        {
+            Type classType = Type.GetType(className);
+            result.AppendLine($"All Private Methods of Class: {className}");
+            result.AppendLine($"Base Class: {classType.BaseType.Name}");
+            var methods = classType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (var method in methods)
+            {
+                if (method.IsPrivate || method.IsFamily)
+                {
+                    result.AppendLine($"{method.Name}");
+                }
+            }
+            return result.ToString().TrimEnd();
+        }
+    }
+
+}
