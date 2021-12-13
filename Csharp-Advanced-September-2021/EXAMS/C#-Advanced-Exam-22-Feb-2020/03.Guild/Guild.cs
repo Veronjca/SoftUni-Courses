3e6d74717368c6317a5b3c21e123486d82d15406
Veronica <veronicagoranova@gmail.com>
2021-12-13 16:56:41 +0200

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/EXAMS/C#-Advanced-Exam-22-Feb-2020/03.Guild/Guild.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> roster;
+
+        public Guild(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.roster = new List<Player>();
+        }
+
+        public string Name { get; private set; }
+        public int Capacity { get; private set; }
+
+        public int Count => this.roster.Count;
+
+        public void AddPlayer(Player player)
+        {
+            if (this.roster.Count < this.Capacity)
+            {
+                this.roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            Player current = this.roster.FirstOrDefault(p => p.Name == name);
+            if (current != null)
+            {
+                this.roster.Remove(current);
+                return true;
+            }
+            return false;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            Player current = this.roster.FirstOrDefault(p => p.Name == name);
+            current.Rank = "Member";
+        }
+
+        public void DemotePlayer(string name)
+        {
+            Player current = this.roster.FirstOrDefault(p => p.Name == name);
+            current.Rank = "Trial";
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            Player[] removed = this.roster.Where(p => p.Class == @class).ToArray();
+            this.roster.RemoveAll(p => p.Class == @class);
+            return removed;
+
+        }
+
+        public string Report()
+        {
+            return $"Players in the guild: {this.Name}{Environment.NewLine}{String.Join(Environment.NewLine, this.roster)}";
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/EXAMS/C#-Advanced-Exam-22-Feb-2020/03.Guild/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public Player(string name, string @class)
+        {
+            this.Name = name;
+            this.Class = @class;
+        }
+
+        public string Name { get; private set; }
+        public string Class { get; private set; }
+        public string Rank { get; set; } = "Trial";
+        public string Description { get; set; } = "n/a";
+
+        public override string ToString()
+        {
+            return $"Player {this.Name}: {this.Class}{Environment.NewLine}Rank: {this.Rank}{Environment.NewLine}Description: {this.Description}";
+        }
+    }
+}
