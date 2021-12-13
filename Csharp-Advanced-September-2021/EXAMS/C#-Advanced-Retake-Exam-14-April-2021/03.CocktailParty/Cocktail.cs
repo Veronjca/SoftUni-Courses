6c28f3d304cf669069875590489fd2756e095d82
Veronica <veronicagoranova@gmail.com>
2021-12-13 21:07:52 +0200

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/EXAMS/C#-Advanced-Retake-Exam-14-April-2021/03.CocktailParty/Cocktail.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            this.Ingredients = new List<Ingredient>();
+            this.Name = name;
+            this.Capacity = capacity;
+            this.MaxAlcoholLevel = maxAlcoholLevel;
+        }
+
+        public List<Ingredient> Ingredients { get; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+        public int CurrentAlcoholLevel => this.Ingredients.Sum(i => i.Alcohol);
+
+        public void Add(Ingredient ingredient)
+        {
+            if (this.Ingredients.Count < this.Capacity && !this.Ingredients.Any(i => i.Name == ingredient.Name) && this.Ingredients.Sum(i => i.Alcohol) < this.MaxAlcoholLevel) 
+            {
+                this.Ingredients.Add(ingredient);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Ingredient current = this.Ingredients.FirstOrDefault(i => i.Name == name);
+            if (current == null)
+            {
+                return false;
+            }
+            this.Ingredients.Remove(current);
+            return true;
+        }
+
+        public Ingredient FindIngredient(string name)
+            => this.Ingredients.FirstOrDefault(i => i.Name == name);
+
+        public Ingredient GetMostAlcoholicIngredient()
+            => this.Ingredients.OrderByDescending(i => i.Alcohol).FirstOrDefault();
+
+        public string Report()
+        {
+            return $"Cocktail: {this.Name} - Current Alcohol Level: {this.CurrentAlcoholLevel}{Environment.NewLine}{string.Join(Environment.NewLine, this.Ingredients)}";
+        }
+    }
+}
---
+++ Csharp-Advanced-September-2021/EXAMS/C#-Advanced-Retake-Exam-14-April-2021/03.CocktailParty/Ingredient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Ingredient
+    {
+        public Ingredient(string name, int alcohol, int quantity)
+        {
+            this.Name = name;
+            this.Alcohol = alcohol;
+            this.Quantity = quantity;
+        }
+        public string Name { get; set; }
+        public int Alcohol { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"Ingredient: {this.Name}{Environment.NewLine}Quantity: {this.Quantity}{Environment.NewLine}Alcohol: {this.Alcohol}";
+        }
+    }
+}
