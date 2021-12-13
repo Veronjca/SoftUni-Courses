bd547d0ab7decc008ead0f0e369ff5582aa25081
Veronica <veronicagoranova@gmail.com>
2021-12-13 20:45:47 +0200

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/EXAMS/C#-Advanced-Retake-Exam-14-April-2021/01.WarmWinter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.WarmWinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> hats = new Stack<int>(ReadArrayFromConsole());
+            Queue<int> scarfs = new Queue<int>(ReadArrayFromConsole());
+
+            List<int> sets = new List<int>();
+            while (hats.Count > 0 && scarfs.Count > 0)
+            {
+                int hat = hats.Pop();
+                int scarf = scarfs.Peek();
+                if (hat > scarf)
+                {
+                    int set = hat + scarf;
+                    scarfs.Dequeue();
+                    sets.Add(set);
+                }
+                else if (scarf == hat)
+                {
+                    scarfs.Dequeue();
+                    hat += 1;
+                    hats.Push(hat);
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.Max()}");
+            Console.WriteLine(string.Join(' ', sets));
+        }
+
+        private static IEnumerable<int> ReadArrayFromConsole()
+        {
+            return Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+        }
+    }
+}
