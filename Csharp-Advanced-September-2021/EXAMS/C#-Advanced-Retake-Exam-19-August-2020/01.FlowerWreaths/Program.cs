d4756119d2a66421870e674e01df94b0259b6ef2
Veronica <veronicagoranova@gmail.com>
2021-12-15 18:54:02 +0200

    Adding solved problem.

---
+++ Csharp-Advanced-September-2021/EXAMS/C#-Advanced-Retake-Exam-19-August-2020/01.FlowerWreaths/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.FlowerWreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(ReadArrayFromConsole());
+            Queue<int> roses = new Queue<int>(ReadArrayFromConsole());
+            int wreaths = 0;
+
+            int storedFlowers = 0;
+
+            while (lilies.Count > 0 && roses.Count > 0)
+            {
+                int rose = roses.Peek();
+                int lili = lilies.Pop();
+                int sum = rose + lili;
+                if (sum == 15)
+                {
+                    wreaths++;
+                    roses.Dequeue();
+                }
+                else if (sum > 15)
+                {
+                    lili-=2;
+                    lilies.Push(lili);
+                    continue;
+                }
+                else
+                {
+                    roses.Dequeue();
+                    storedFlowers += sum;
+                }
+            }
+
+            wreaths += Math.Abs(storedFlowers / 15);
+            Console.WriteLine(wreaths >= 5 ? $"You made it, you are going to the competition with {wreaths} wreaths!" : $"You didn't make it, you need {5 - wreaths} wreaths more!");
+        }
+
+        private static IEnumerable<int> ReadArrayFromConsole()
+        {
+            return Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+        }
+    }
+}
