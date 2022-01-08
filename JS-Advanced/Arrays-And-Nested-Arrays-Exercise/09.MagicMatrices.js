4adccb08d1a5d1a1e0f3febd49ba0652dc8a8bf6
Veronica <veronicagoranova@gmail.com>
2022-01-09 01:35:02 +0200

    update

--- JS-Advanced/Arrays-And-Nested-Arrays-Exercise/09.MagicMatrices.js
+++ JS-Advanced/Arrays-And-Nested-Arrays-Exercise/09.MagicMatrices.js
@@ -1,5 +1,5 @@
 function magic(matrix){
-    let rowSum = matrix[0].reduce((a,b) => a+b);
+    let sum = matrix[0].reduce((a,b) => a+b);
     
     for (let i = 0; i < matrix.length; i++) {
         let currentRowSum = matrix[i].reduce((a,b) => a+b);
@@ -8,7 +8,7 @@ function magic(matrix){
         for (let j = 0; j < matrix.length; j++) {
             currentColSum += matrix[j][i];         
         } 
-        if(currentRowSum != rowSum || currentColSum != rowSum){
+        if(currentRowSum != sum || currentColSum != sum){
             return false;
         } 
     }
