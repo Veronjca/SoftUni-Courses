08cc0b98d8dba7f414417d7f6cf455bf3d3211f2
Veronica <veronicagoranova@gmail.com>
2022-02-08 23:01:48 +0200

    Adding solved problem.

--- JS-Advanced/Classes-Аnd-Attributes-Exercise/11. View-Model/app.js
+++ JS-Advanced/Classes-Аnd-Attributes-Exercise/11. View-Model/app.js
@@ -1,6 +1,37 @@
 class Textbox {
-    //TODO
+  constructor(selector, regex) {
+    this._elements = document.querySelectorAll(`${selector}`);
+    this._invalidSymbols = regex;
+    this._value = '';
+  }
+
+  get elements() {
+    return this._elements;
+  }
+
+
+  get value() {
+    return this._value;
+  }
+
+  set value(param) {
+    this._value = param;
+    let arr = Array.from(this.elements);
+    arr.forEach((element) => {
+      element.value = this._value;
+    });
+  }
+
+  isValid() {
+      if (this._invalidSymbols.test(this._value)) {
+        return false;
+      }
+    return true;
+  }
 }
 
-let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
-let inputs = document.getElementsByClassName('.textbox');
+let textbox = new Textbox(".textbox", /[^a-zA-Z0-9]/);
+let inputs = Array.from(document.getElementsByClassName("textbox"));
+textbox.value = 'asd_';
+
+
