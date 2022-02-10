afedfd0b7af7f5cb04e37231d61b2723919c5585
Veronica <veronicagoranova@gmail.com>
2022-02-10 17:52:33 +0200

    spare line deleted

--- JS-Advanced/Prototypes-And-Inheritance-Exercise/02.StringExtension.js
+++ JS-Advanced/Prototypes-And-Inheritance-Exercise/02.StringExtension.js
@@ -17,7 +17,6 @@
     } else if (this.length > n) {
       let index = this.valueOf(this).substring(0, n -2).lastIndexOf(" ");
       if (index !== -1) {
-        let arr = this.slice(0,6);
         return this.slice(0, index) + '...';
       } else if (n >= 4) {
         return this.slice(0, n - 3) + '...';
@@ -34,27 +33,6 @@
     }
     return string;
   };
-  let str = 'my string';
-str = str.ensureStart('my');
-console.log(str);
-str = str.ensureStart('hello ');
-console.log(str);
-str = str.truncate(16);
-console.log(str);
-str = str.truncate(14);
-console.log(str);
-str = str.truncate(8);
-console.log(str);
-str = str.truncate(4);
-console.log(str);
-str = str.truncate(2);
-console.log(str);
-str = String.format('The {0} {1} fox',
-  'quick', 'brown');
-  console.log(str);
-str = String.format('jumps {0} {1}',
-  'dog');
-  console.log(str);
 })();
 
 
