5d1d557874009c146bae73f473bffabdf29e98ef
Veronica <veronicagoranova@gmail.com>
2022-06-24 23:57:30 +0300

    Adding solved problems.

---
+++ Entity-Framework-Core/EntityFrameworkIntroduction/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context)); 
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees.Select(x => new
+            {
+                EmployeeId = x.EmployeeId,
+                FirstName = x.FirstName,
+                MiddleName = x.MiddleName,
+                LastName = x.LastName,
+                JobTitle = x.JobTitle,
+                Salary = x.Salary
+            })
+            .OrderBy(x => x.EmployeeId)
+            .ToList();
+
+            StringBuilder result = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                result.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+            return result.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees.Select(x => new
+            {
+                FirstName = x.FirstName,
+                Salary = x.Salary
+            })
+            .Where(x => x.Salary > 50000)
+            .OrderBy(x => x.FirstName)
+            .ToList();
+
+            StringBuilder result = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                result.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+            return result.ToString().TrimEnd();
+        }
+
+    }  
+}
